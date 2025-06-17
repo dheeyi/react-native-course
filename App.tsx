dadeb6df594aab2af680e6678d1a8a0e05efe590
William Barra <wbarra-ext@wondrhealth.com>
2025-06-17 09:18:52 -0400

    AC-9 Adds tabs

--- App.tsx
+++ App.tsx
@@ -3,22 +3,26 @@ import { StyleSheet, View } from 'react-native';
 import {colors} from './src/config/colors.ts';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from './src/screens/Slider';
 import ProfileScreen from './src/screens/Profile';
 import WishlistScreen from './src/screens/Wishlist';
+import SearchScreen from './src/screens/Search';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App(): React.JSX.Element {
   return (
     <View style={styles.mainContainer}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Profile" component={ProfileScreen} />
-          <Stack.Screen name="Wishlist" component={WishlistScreen} />
-        </Stack.Navigator>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Profile" component={ProfileScreen} />
+          <Tab.Screen name="Wishlist" component={WishlistScreen} />
+          <Tab.Screen name="Search" component={SearchScreen} />
+        </Tab.Navigator>
       </NavigationContainer>
     </View>
   );
